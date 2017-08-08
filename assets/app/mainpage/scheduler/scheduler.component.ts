6e012cf4a1f753403c66991417d4ff10687bab7b
pkim <pkim@regulatedconsultants.com>
2017-08-08 16:26:15 -0500

    favicon + scheduler

--- assets/app/mainpage/scheduler/scheduler.component.ts
+++ assets/app/mainpage/scheduler/scheduler.component.ts
@@ -1,10 +1,18 @@
 
 import {Component} from "@angular/core";
 @Component({
-    selector: 'app-scheduler',
-    templateUrl: './scheduler.component.html'
+    selector: "app-scheduler",
+    templateUrl: "./scheduler.component.html",
+    styleUrls: ["./scheduler.component.css"]
 })
 
 export class SchedulerComponent {
+    dates = [{"day" : "Monday", "id": 1},
+            {"day" : "Tuesday", "id": 2},
+            {"day" : "Wednesday", "id": 3},
+            {"day" : "Thursday", "id": 4},
+            {"day" : "Friday", "id":  5},
+            {"day" : "Saturday", "id":  6},
+            {"day" : "Sunday", "id": 7}];
 
 }
