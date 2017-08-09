afc55cf47a231ced7369c8fb163a3f721290b1f2
pkim <pkim@regulatedconsultants.com>
2017-08-09 13:19:21 -0500

    Backup before merge. Can create a schedule based on data

--- assets/app/mainpage/scheduler/scheduler.component.ts
+++ assets/app/mainpage/scheduler/scheduler.component.ts
@@ -1,5 +1,5 @@
 
-import {Component, OnInit} from "@angular/core";
+import {Component, Input, AfterViewInit} from "@angular/core";
 @Component({
     selector: "app-scheduler",
     templateUrl: "./scheduler.component.html",
@@ -15,7 +15,7 @@ export class SchedulerComponent {
             {"day" : "Saturday", "id":  6},
             {"day" : "Sunday", "id": 7}];
 
-    tempSchedule = [
+    @Input() tempPreferredSchedule = [
         {
             "work": "White Windmill",
             "schedule":
@@ -23,24 +23,66 @@ export class SchedulerComponent {
                 {
                     "day": 1,
                     "start": 10,
-                    "duration": 8
+                    "duration": 5
                 },
                 {
                     "day": 2,
                     "start": 14,
-                    "duraton": 4
+                    "duration": 4
+                }
+            ]
+        }
+        ,
+        {
+            "work": "Jang Su Jang",
+            "schedule":
+            [
+                {
+                    "day": 3,
+                    "start": 8,
+                    "duration": 5
+                },
+                {
+                    "day": 4,
+                    "start": 16,
+                    "duration": 6
+                },
+                {
+                    "day": 6,
+                    "start": 14,
+                    "duration": 8
+                }
+            ]
+        },
+        {
+            "work": "Emily J",
+            "schedule":
+            [
+                {
+                    "day": 1,
+                    "start": 17,
+                    "duration": 2
                 }
             ]
         }
     ];
 
-    ngOnInit(): void {
-        for (let i : number = 0; i < this.tempSchedule[0].schedule.length; i++) {
-            let day : number = this.tempSchedule[0].schedule[i].day;
-            let start : number = this.tempSchedule[0].schedule[i].start - 7;
-            let targetID : string = start.toString() + "_" + day.toString();
-            console.log(targetID);
-            console.log(document.getElementById(targetID));
+    colorSchemaArray : Array<string> = ["#A8185F", "#E7D016", "#18A819"];
+
+    ngAfterViewInit(): void {
+        for (let i : number = 0; i < this. tempPreferredSchedule.length; i++) {
+            for (let j : number = 0; j < this.tempPreferredSchedule[i].schedule.length; j++) {
+                let day : number = this.tempPreferredSchedule[i].schedule[j].day;
+                let start : number = this.tempPreferredSchedule[i].schedule[j].start - 7;
+                let duration : number = this.tempPreferredSchedule[i].schedule[j].duration;
+
+                for (let offset : number = 0; offset < duration; offset++) {
+                    let targetID : string = (start + offset).toString() + "_" + (day).toString();
+                    document.getElementById(targetID).style.backgroundColor = this.colorSchemaArray[i];
+                }
+            }
         }
     }
+
+    // ngOnChanges(): 
 }
