6258b7176b8d0270a5352a027267909a71be827f
pkim <pkim@regulatedconsultants.com>
2017-08-17 08:57:56 -0500

    Scheduler has radio buttons that users can use to toggle between actual vs preferred schedule ( todo )

--- assets/app/mainpage/logout.component.ts
+++ assets/app/mainpage/logout.component.ts
@@ -5,12 +5,12 @@ import {Router} from "@angular/router";
     selector: 'app-logout',
     template: `
     <div style="height: 90vh">
-    <div style="margin:2% 4% 2% 4%;border: 0.3px solid #07889B; background-color:rgba(5,91,104,0); height: 50vh">
+    <div style="margin:2% 4% 2% 4%;border: 0.3px solid #07889B; background-color:rgba(5,91,104,0); height: 45vh">
         <div class="header" style="background-color:#07889B; color:azure; height:40px;">
             <p style="font-size: 20px; padding-top:0.4%; margin-left: 3%">Are you sure you want to leave us?😢</p>
         </div>
         <div style='text-align:center;'>
-            <button class="btn btn-warning" style='margin: auto; margin-top: 5%; width:150px; height:50px; display:block' (click)="onLogout()">
+            <button class="btn btn" style='margin: auto; background-color: rgba(168, 24, 95, 0.5); margin-top: 5%; width:150px; height:50px; display:block' (click)="onLogout()">
                 😈
                 Yes
             </button>
