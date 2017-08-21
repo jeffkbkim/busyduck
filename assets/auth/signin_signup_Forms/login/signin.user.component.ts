158c9a56e4e3035bbc5a272ea1fa1e6dc701dd6a
KANG BIN KIM <kimkb2011@gmail.com>
2017-08-21 18:50:25 -0400

    Merge branch 'mainpage' into tempBranch
    # Conflicts:
    #	assets/auth/signin_signup_Forms/login/signin.user.component.ts

--- assets/auth/signin_signup_Forms/login/signin.user.component.ts
+++ assets/auth/signin_signup_Forms/login/signin.user.component.ts
@@ -36,7 +36,11 @@ export class SigninUserComponent implements OnInit {
 
     onSubmit() {
         const user = new User(this.registerForm.value.email, this.registerForm.value.password, this.isAdmin);
+<<<<<<< HEAD
         console.log(this.isAdmin);
+=======
+
+>>>>>>> mainpage
         this.authService.signin(user)
             .subscribe(
                 data => {
